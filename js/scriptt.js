68c4f0afe402fcc363727ef0fd8cd74f7c6a0159
nura92 <131611344+nura92@users.noreply.github.com>
2023-07-31 04:24:27 -0700

    this login part

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,11 +1,21 @@
 let wraped = document.querySelector(".wrapper");
 let login = document.querySelector(".login-link");
-console.log(login)
 let register = document.querySelector(".register-link");
-console.log(register);
+const btn = document.querySelector(".login-popup");
+const clos = document.querySelector(".close-icon");
+
+
+console.log(clos);
 register.addEventListener("click", () => {
-    wraped.classList.add('active')
-})
+	wraped.classList.add("active");
+});
 login.addEventListener("click", () => {
 	wraped.classList.remove("active");
 });
+btn.addEventListener("click", () => {
+	wraped.classList.add("pop-active");
+});
+clos.addEventListener("click", () => {
+	wraped.classList.remove("pop-active");
+});
+
